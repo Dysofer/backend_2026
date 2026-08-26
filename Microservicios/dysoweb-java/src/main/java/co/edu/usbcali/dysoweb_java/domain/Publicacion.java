84ed3674c2ea20ad303c4bda6edb0e1415ce7a4d
Dysofer <dylanalexander.solisfernandez.27@gmail.com>
2026-08-25 19:16:58 -0500

    Agregando entidad publicaciones

--- Microservicios/dysoweb-java/src/main/java/co/edu/usbcali/dysoweb_java/domain/Publicacion.java
+++ Microservicios/dysoweb-java/src/main/java/co/edu/usbcali/dysoweb_java/domain/Publicacion.java
@@ -1,4 +1,41 @@
 package co.edu.usbcali.dysoweb_java.domain;
 
+import co.edu.usbcali.dysoweb_java.domain.enums.PrivacidadPublicacion;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Publicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Usuario autor;
+
+    @Column(name = "contenido")
+    private String contenido;
+
+    @Column(name = "privacidad", nullable = false)
+    private PrivacidadPublicacion privacidad;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
 }
